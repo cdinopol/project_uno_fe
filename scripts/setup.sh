d2849610fe0c6f40adb200e6faabf63925051cb9
Carlo Activ <carlo@activ.com.sg>
2020-03-23 16:52:44 +0800

    relocated template files

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,11 +3,11 @@
 source env.sh
 
 #copy files and replace variables
-cd $ROOT_DIR/build/jsb-link/frameworks/runtime-src/proj.android-studio/app/res/values && 
-sed "s/__GOOGLE_APP_ID__/$GOOGLE_APP_ID/g; s/__FB_APP_ID__/$FB_APP_ID/g" strings.xml.template > strings.xml
+sed "s/__GOOGLE_APP_ID__/$GOOGLE_APP_ID/g; s/__FB_APP_ID__/$FB_APP_ID/g" templates/strings.xml > \
+../build/jsb-link/frameworks/runtime-src/proj.android-studio/app/res/values/strings.xml
 
-cd $ROOT_DIR/build/jsb-link/frameworks/runtime-src/proj.android-studio/app &&
-sed "s/__FB_APP_ID__/$FB_APP_ID/g" AndroidManifest.xml.template > AndroidManifest.xml
+sed "s/__FB_APP_ID__/$FB_APP_ID/g" templates/AndroidManifest.xml > \
+../build/jsb-link/frameworks/runtime-src/proj.android-studio/app/AndroidManifest.xml
 
-cd $ROOT_DIR/settings &&
-sed "s/__XXTEA_KEY__/$XXTEA_KEY/g" builder.json.template > builder.json
+sed "s/__XXTEA_KEY__/$XXTEA_KEY/g" builder.json > \
+../settings/builder.json
