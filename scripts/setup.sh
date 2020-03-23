1bad990c551c81fe0b550ac1979b6c8279463d2f
Carlo Activ <carlo@activ.com.sg>
2020-03-23 20:54:39 +0800

    keep strings.xml as dynamic file

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,9 +5,3 @@ source env.sh
 #copy files and replace variables
 sed "s/__GOOGLE_APP_ID__/$GOOGLE_APP_ID/g; s/__FB_APP_ID__/$FB_APP_ID/g" templates/strings.xml > \
 ../build/jsb-link/frameworks/runtime-src/proj.android-studio/app/res/values/strings.xml
-
-sed "s/__FB_APP_ID__/$FB_APP_ID/g" templates/AndroidManifest.xml > \
-../build/jsb-link/frameworks/runtime-src/proj.android-studio/app/AndroidManifest.xml
-
-sed "s/__XXTEA_KEY__/$XXTEA_KEY/g" templates/builder.json > \
-../settings/builder.json
