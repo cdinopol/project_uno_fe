2684af478c75008f451b591c6773d452b2ca1d46
Carlo Activ <carlo@activ.com.sg>
2020-03-23 16:16:33 +0800

    setup script cleanup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,16 +2,12 @@
 
 source env.sh
 
-rm $ROOT_DIR/build/jsb-link/frameworks/runtime-src/proj.android-studio/app/res/values/strings.xml
-rm $ROOT_DIR/build/jsb-link/frameworks/runtime-src/proj.android-studio/app/AndroidManifest.xml
-rm $ROOT_DIR/settings/builder.json
+copy files and replace variables
+cd $ROOT_DIR/build/jsb-link/frameworks/runtime-src/proj.android-studio/app/res/values && 
+sed "s/__GOOGLE_APP_ID__/$GOOGLE_APP_ID/g; s/__FB_APP_ID__/$FB_APP_ID/g" strings.xml.template > strings.xml
 
-# copy files and replace variables
-# cd $ROOT_DIR/build/jsb-link/frameworks/runtime-src/proj.android-studio/app/res/values && 
-# sed "s/__GOOGLE_APP_ID__/$GOOGLE_APP_ID/g; s/__FB_APP_ID__/$FB_APP_ID/g" strings.xml.template > strings.xml
+cd $ROOT_DIR/build/jsb-link/frameworks/runtime-src/proj.android-studio/app &&
+sed "s/__FB_APP_ID__/$FB_APP_ID/g" AndroidManifest.xml.template > AndroidManifest.xml
 
-# cd $ROOT_DIR/build/jsb-link/frameworks/runtime-src/proj.android-studio/app &&
-# sed "s/__FB_APP_ID__/$FB_APP_ID/g" AndroidManifest.xml.template > AndroidManifest.xml
-
-# cd $ROOT_DIR/settings &&
-# sed "s/__XXTEA_KEY__/$XXTEA_KEY/g" builder.json.template > builder.json
+cd $ROOT_DIR/settings &&
+sed "s/__XXTEA_KEY__/$XXTEA_KEY/g" builder.json.template > builder.json
