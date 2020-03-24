93645106b1182c3a2debb3b9254e0f0abad02eec
Carlo Activ <carlo@activ.com.sg>
2020-03-24 09:48:57 +0800

    readme update, setup verbose

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,5 +3,8 @@
 source env.sh
 
 #copy files and replace variables
+echo "creating strings.xml..."
 sed "s/__GOOGLE_APP_ID__/$GOOGLE_APP_ID/g; s/__FB_APP_ID__/$FB_APP_ID/g" templates/strings.xml > \
 ../build/jsb-link/frameworks/runtime-src/proj.android-studio/app/res/values/strings.xml
+
+echo "Done!"
